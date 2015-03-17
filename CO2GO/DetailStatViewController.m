de58ef305b7a5617501a52c099cd995a9618e248
Kamil Khan <kamil.z.khan@gmail.com>
2015-03-16 17:21:39 -0700

    now includes units

--- CO2GO/DetailStatViewController.m
+++ CO2GO/DetailStatViewController.m
@@ -29,10 +29,10 @@
     [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
     
     [self.date_label setText:[NSString stringWithFormat:@"%@", [dateFormatter stringFromDate:self.trip.date]]];
-    [self.distance_label setText:[NSString stringWithFormat:@"%.02f", self.trip.distance]];
-    [self.emissions_label setText:[NSString stringWithFormat:@"%.02f", self.trip.emissions]];
+    [self.distance_label setText:[NSString stringWithFormat:@"%.02f km", self.trip.distance]];
+    [self.emissions_label setText:[NSString stringWithFormat:@"%.02f g", self.trip.emissions]];
     [self.vehicle_label setText:self.trip.vehicle];
-    [self.interval_label setText:[NSString stringWithFormat:@"%f", self.trip.timeElapsed]];
+    [self.interval_label setText:[NSString stringWithFormat:@"%.02f mins", (self.trip.timeElapsed /60)]];
     NSArray *allStats = [[StatStore sharedStore] allStats];
     double co = 0;
     double dist = 0;
@@ -47,10 +47,11 @@
     if (diffAvg <= 0) {
         self.tripCO2Avg.textColor = [[UIColor alloc] initWithRed: 54./255 green: 190./255 blue:32./255 alpha:1];
     } else {
-        self.tripCO2Avg.textColor = [[UIColor alloc] initWithRed:234/255 green:38/255 blue:24/255 alpha:1];
+        self.tripCO2Avg.textColor = [[UIColor alloc] initWithRed:234./255 green:38./255 blue:24./255 alpha:1];
     }
     [self.tripCO2Avg setText:[NSString stringWithFormat:@"%.02f", myAvg]];
     [self.historicalCO2Avg setText:[NSString stringWithFormat:@"%.02f", histAvg]];
+    self.tripCO2Avg.textColor = [[UIColor alloc] initWithRed:1 green:1 blue:1 alpha: 1];
 }
 
 - (void)didReceiveMemoryWarning {
